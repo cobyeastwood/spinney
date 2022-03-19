8b35ab541033bf2b4e39784bfccac1463f47a894
Coby Eastwood <61709523+cobyeastwood@users.noreply.github.com>
2022-03-19 16:13:23 -0700

    first

---
+++ src/Parse.js
@@ -0,0 +1,89 @@
+export class Parse {
+	constructor(options) {
+		this.root = null;
+		this.seen = new Set();
+
+		this.setUp(options);
+	}
+
+	setUp({ data, options }) {
+		if (typeof Buffer != 'undefined' && Buffer.isBuffer(data)) {
+			this.root = data.toString();
+			return;
+		}
+
+		if (typeof data === 'string') {
+			this.root = htmlparser2.parseDocument(data, options);
+			return;
+		}
+	}
+
+	find(target, root = this.root) {
+		return this.transverse(root).filter(type => type === target);
+	}
+
+	transverse(root) {
+		let elements = [];
+		let stack = [root];
+
+		while (stack.length) {
+			let node = stack.pop();
+
+			if (node && !node.type) {
+				continue;
+			}
+
+			if (this.seen.has(node)) {
+				continue;
+			}
+
+			this.seen.add(node);
+
+			elements.push(node.type);
+
+			if (node.prev) {
+				stack.push(this.transversePrev(node.prev));
+			}
+
+			if (node.next) {
+				stack.push(this.transverseNext(node.next));
+			}
+
+			for (const key in node.children) {
+				stack.push(node.children[key]);
+			}
+		}
+
+		return elements;
+	}
+
+	transversePrev(element) {
+		if (!element) {
+			return element;
+		}
+
+		let neighbors = [];
+
+		while (element.prev) {
+			neighbors.push(element);
+			element = element.prev;
+		}
+
+		return neighbors;
+	}
+
+	transverseNext(element) {
+		if (!element) {
+			return element;
+		}
+
+		let neighbors = [];
+
+		while (element.next) {
+			neighbors.push(element);
+			element = element.next;
+		}
+
+		return neighbors;
+	}
+}

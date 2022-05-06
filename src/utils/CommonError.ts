6cad99a8912f869e6257b2f1edaae7a311133926
Coby Eastwood <61709523+cobyeastwood@users.noreply.github.com>
2022-05-05 22:57:33 -0700

    fix: error msgs and debug

--- src/utils/CommonError.ts
+++
@@ -1,7 +0,0 @@
-export default class CommonError extends Error {
-	constructor(func: Function, parameter: any) {
-		super(
-			`${func.name} received unexpected parameter of type ${typeof parameter}`
-		);
-	}
-}
--- src/utils/StringWriter.ts
+++ src/utils/StringWriter.ts
@@ -9,12 +9,8 @@ export default class StringWriter {
 		this.decode = new StringDecoder();
 	}
 
-	isBuffer(encoding: string): boolean {
-		return encoding === 'buffer';
-	}
-
 	write(chunk: any, encoding: string) {
-		if (this.isBuffer(encoding)) {
+		if (encoding === 'buffer') {
 			this.string += this.decode.write(chunk);
 		} else {
 			this.string += chunk;
